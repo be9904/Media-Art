9fc20a3eb1689e5322892b535af1882c6b95658c
be9904 <be9904@gmail.com>
2022-08-16 23:21:47 +0900

    add break animation

--- Assets/Media-Art/JH/Scripts/PiggyBank.cs
+++ Assets/Media-Art/JH/Scripts/PiggyBank.cs
@@ -8,19 +8,36 @@ public class PiggyBank : MonoBehaviour
 {
     public int CoinCapacity;
 
+    private MeshRenderer _meshRenderer;
     private Rigidbody _rigidbody;
     private Grabbable _grabbable;
+
+    public GameObject emptyAnimation;
+    public GameObject fullAnimation;
     
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _grabbable = GetComponent<BNG.Grabbable>();
+        _meshRenderer = GetComponent<MeshRenderer>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Hammer"))
+        if (collision.gameObject.CompareTag("Hammer"))
+        {
             Debug.Log(collision.gameObject.name);
+            PlayAnimation(0);
+        }
+    }
+    
+    void PlayAnimation(int id)
+    {
+        _meshRenderer.enabled = false;
+        if (id == 0) // empty
+            emptyAnimation.SetActive(true);
+        else if (id == 1) // full
+            fullAnimation.SetActive(true);
     }
 }
