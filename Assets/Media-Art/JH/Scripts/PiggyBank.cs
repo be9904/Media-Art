4d7fb5e4103bf34d759f4105a35a7a427d273c17
be9904 <be9904@gmail.com>
2022-08-17 12:08:38 +0900

    add smash sfx

--- Assets/Media-Art/JH/Scripts/PiggyBank.cs
+++ Assets/Media-Art/JH/Scripts/PiggyBank.cs
@@ -14,10 +14,18 @@ public class PiggyBank : MonoBehaviour
 
     public GameObject emptyAnimation;
     public GameObject fullAnimation;
+    public AudioClip breakEffect;
+
+    private AudioSource _audioSource;
+    private AudioClip _originalSfx;
+    public bool isHit = false;
     
     // Start is called before the first frame update
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
+        _originalSfx = _audioSource.clip;
+        
         _rigidbody = GetComponent<Rigidbody>();
         _grabbable = GetComponent<BNG.Grabbable>();
         _meshRenderer = GetComponent<MeshRenderer>();
@@ -25,7 +33,7 @@ public class PiggyBank : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Hammer"))
+        if (collision.gameObject.CompareTag("Hammer") && !isHit)
         {
             if(CoinCapacity < 20)
                 PlayAnimation(0);
@@ -36,10 +44,28 @@ public class PiggyBank : MonoBehaviour
     
     void PlayAnimation(int id)
     {
+        isHit = true;
+        
         _meshRenderer.enabled = false;
+        _rigidbody.isKinematic = true;
+
+        _audioSource.clip = breakEffect;
+        _audioSource.Play();
+        
         if (id == 0) // empty
             emptyAnimation.SetActive(true);
         else if (id == 1) // full
             fullAnimation.SetActive(true);
+
+        StartCoroutine(ResetAudioClip());
+    }
+
+    IEnumerator ResetAudioClip()
+    {
+        yield return new WaitForSeconds(breakEffect.length + 2);
+
+        _audioSource.clip = _originalSfx;
+        _rigidbody.isKinematic = false;
+        isHit = false;
     }
 }
